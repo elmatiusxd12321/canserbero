2de8612a9b4fb4c269a82ea41d332c81fc541141
Gamematweb <118083998+elmatiusxd12321@users.noreply.github.com>
2023-01-18 14:24:49 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+///español///
+function español()
+{
+
+window.location = "index.html"
+
+}
+///ingles///
+function ingles()
+{
+
+window.location = "index2.html"
+
+}
+///japones///
+function japones()
+{
+
+window.location = "index3.html"
+
+}
+///chino///
+function chino()
+{
+
+window.location = "index4.html"
+
+}
